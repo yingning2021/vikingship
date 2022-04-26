720cae9c3c4554eeb04973cf8452775a0c18ede4
yingning2021 <yingning2021@163.com>
2022-04-26 12:29:23 +0800

    class: 完成button的样式的开发

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import './styles/index.scss'
-import Button, {ButtonSize, ButtonType} from "./components/button";
+import Button, {ButtonSize, ButtonType} from "./components/Button/button";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Button disabled>Hello</Button>
-        <Button btnType={ButtonType.Primary} size={ButtonSize.Large}>World</Button>
+        <Button >Hello</Button>
+        <Button size={ButtonSize.Large}>World</Button>
+        <Button btnType={ButtonType.Primary} size={ButtonSize.Small}>World</Button>
+        <Button btnType={ButtonType.Danger} size={ButtonSize.Large}>World</Button>
         <Button disabled btnType={ButtonType.Link} href="https://www.baidu.com">Baidu Link</Button>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
